8a223b45c8970b5beb5b215b7a0a69578ed4d3ce
Victor Blanco <vblanco20-1@hotmail.com>
2020-08-14 21:14:39 +0200

    Revert terrain wireframe left there for experimentation

--- client/Rendering/ClientRenderer.cpp
+++ client/Rendering/ClientRenderer.cpp
@@ -193,7 +193,6 @@ void ClientRenderer::Render()
             pipelineDesc.states.depthStencilState.depthFunc = Renderer::ComparisonFunc::COMPARISON_FUNC_LESS;
 
             // Rasterizer state
-            pipelineDesc.states.rasterizerState.fillMode = Renderer::FILL_MODE_WIREFRAME;
             pipelineDesc.states.rasterizerState.cullMode = Renderer::CullMode::CULL_MODE_BACK;
             pipelineDesc.states.rasterizerState.frontFaceMode = Renderer::FrontFaceState::FRONT_FACE_STATE_COUNTERCLOCKWISE;
 
@@ -292,7 +291,6 @@ void ClientRenderer::Render()
             pipelineDesc.states.depthStencilState.depthFunc = Renderer::ComparisonFunc::COMPARISON_FUNC_EQUAL;
 
             // Rasterizer state
-            pipelineDesc.states.rasterizerState.fillMode = Renderer::FILL_MODE_WIREFRAME;
             pipelineDesc.states.rasterizerState.cullMode = Renderer::CullMode::CULL_MODE_BACK;
             pipelineDesc.states.rasterizerState.frontFaceMode = Renderer::FrontFaceState::FRONT_FACE_STATE_COUNTERCLOCKWISE;
 
