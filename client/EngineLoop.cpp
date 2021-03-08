a6c116b7279dbd5311afceb36c83d17e0b204e52
Andreas Vennström <6706447+avennstrom@users.noreply.github.com>
2021-03-08 22:21:07 +0100

    fix compilation on lastest Visual Studio

--- client/EngineLoop.cpp
+++ client/EngineLoop.cpp
@@ -629,7 +629,7 @@ void EngineLoop::DrawMapStats()
         selectedMap = mapNames[0];
 
     selectedMapToLower.resize(selectedMap->length());
-    std::transform(selectedMap->begin(), selectedMap->end(), selectedMapToLower.begin(), std::tolower);
+    std::transform(selectedMap->begin(), selectedMap->end(), selectedMapToLower.begin(), [](char c) { return std::tolower((int)c); });
 
     // Map Selection
     {
@@ -641,7 +641,7 @@ void EngineLoop::DrawMapStats()
         ImGui::InputText("Filter", &searchText);
 
         searchTextToLower.resize(searchText.length());
-        std::transform(searchText.begin(), searchText.end(), searchTextToLower.begin(), std::tolower);
+        std::transform(searchText.begin(), searchText.end(), searchTextToLower.begin(), [](char c) { return std::tolower((int)c); });
 
         bool hasFilter = searchText.length() != 0;
 
@@ -654,7 +654,7 @@ void EngineLoop::DrawMapStats()
             for (const std::string* mapName : mapNames)
             {
                 mapNameCopy.resize(mapName->length());
-                std::transform(mapName->begin(), mapName->end(), mapNameCopy.begin(), std::tolower);
+                std::transform(mapName->begin(), mapName->end(), mapNameCopy.begin(), [](char c) { return std::tolower((int)c); });
 
                 if (mapNameCopy.find(searchTextToLower) == std::string::npos)
                     continue;
@@ -686,7 +686,7 @@ void EngineLoop::DrawMapStats()
                     for (const std::string* mapName : mapNames)
                     {
                         mapNameCopy.resize(mapName->length());
-                        std::transform(mapName->begin(), mapName->end(), mapNameCopy.begin(), std::tolower);
+                        std::transform(mapName->begin(), mapName->end(), mapNameCopy.begin(), [](char c) { return std::tolower((int)c); });
 
                         if (mapNameCopy.find(searchTextToLower) == std::string::npos)
                             continue;
