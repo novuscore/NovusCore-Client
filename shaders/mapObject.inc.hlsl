6432b91e79c01363b70c291ec9e718779ff2f4dc
NixAJ <nickiarpejensen@gmail.com>
2021-07-18 18:52:03 +0200

    Fixed MapObject Vertex Lighting
    Renamed a few variables to be consistent

--- shaders/mapObject.inc.hlsl
+++ shaders/mapObject.inc.hlsl
@@ -5,8 +5,8 @@ struct PackedInstanceLookupData
     uint packed1; // uint16_t cullingDataID, uint16_t vertexColorTextureID0
     uint packed2; // uint16_t vertexColorTextureID1, uint16_t padding
     uint vertexOffset;
+    uint vertexColor0Offset;
     uint vertexColor1Offset;
-    uint vertexColor2Offset;
     uint loadedObjectID;
 }; // 28 bytes
 
@@ -18,8 +18,8 @@ struct InstanceLookupData
     uint vertexColorTextureID0;
     uint vertexColorTextureID1;
     uint vertexOffset;
+    uint vertexColor0Offset;
     uint vertexColor1Offset;
-    uint vertexColor2Offset;
     uint loadedObjectID;
 };
 
@@ -36,6 +36,10 @@ InstanceLookupData LoadInstanceLookupData(uint instanceLookupDataID)
     instanceLookupData.cullingDataID = packedInstanceLookupData.packed1 & 0xFFFF;
     instanceLookupData.vertexColorTextureID0 = (packedInstanceLookupData.packed1 >> 16) & 0xFFFF;
     instanceLookupData.vertexColorTextureID1 = packedInstanceLookupData.packed2 & 0xFFFF;
+    instanceLookupData.vertexOffset = packedInstanceLookupData.vertexOffset;
+    instanceLookupData.vertexColor0Offset = packedInstanceLookupData.vertexColor0Offset;
+    instanceLookupData.vertexColor1Offset = packedInstanceLookupData.vertexColor1Offset;
+    instanceLookupData.loadedObjectID = packedInstanceLookupData.loadedObjectID;
     
     return instanceLookupData;
 }
